244d5f522776a29189033021dc47cb718bc5a5b8
chenmy1903 <chenmy10@outlook.com>
2022-04-15 09:55:06 +0800

    更新注释

--- src/com/nkwjg/loopmethod/DoWhileMethod.java
+++ src/com/nkwjg/loopmethod/DoWhileMethod.java
@@ -1,3 +1,11 @@
+/**
+ * do...while循环示例
+ * 语法:
+ * do {
+ *     <流程>
+ * } while (表达式); //一定要加分号
+ * 其中流程至少被执行一遍, 直到表达式为false才会跳出循环(也可以用break跳出)
+ */
 package com.nkwjg.loopmethod;
 
 public class DoWhileMethod {
@@ -13,7 +21,7 @@ public class DoWhileMethod {
 
     public static void main(String[] args) {
         try {
-            new DoWhileMethod().run();
+            new DoWhileMethod().run(); //运行主方法
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("程序运行时出现了错误, 错误报告如上, 如果这个错误并不是人为触发(按Ctrl+C之类), 可以去下面的网址报告这个Bug");
